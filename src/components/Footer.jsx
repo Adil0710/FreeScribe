1e87304e27835645ea711b5c678dffdcf81c8197
Adil Patel <91872021+Adil0710@users.noreply.github.com>
2024-06-06 17:38:07 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <header className='flex items-center justify-between gap-4 p-3'>
-            <a href="https://adil0710.github.io/" target='_blank'><h1 className='font-medium text-sm text-slate-400'>Copyright © Developed By |<span className='text-slate-600 bold hover:text-slate-800 duration-200'> Adil Patel</span></h1></a>
+            <a href="https://adil0710.github.io/" target='_blank'><h1 className='text-[10px] font-medium sm:text-sm text-slate-400'>Copyright © Developed By |<span className='text-slate-600 bold hover:text-slate-800 duration-200'> Adil Patel</span></h1></a>
             <div className='gap-4 flex items-center '>
                 
                 <a href="https://www.instagram.com/code_with_adil?igsh=NTc2dTRudnc1ZnYz" target='_blank' className='flex items-center specialBtni px-2 py-1 text-slate-500 rounded-full duration-200 hover:text-rose-400'>
