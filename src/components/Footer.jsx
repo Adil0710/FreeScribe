1cc630951d6ce4f4f87eefe8e7eb40c3dcb44f74
Adil0710 <padil2246@gmail.com>
2024-06-06 16:48:01 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <header className='flex items-center justify-between gap-4 p-3'>
+            <a href="https://adil0710.github.io/" target='_blank'><h1 className='font-medium text-sm text-slate-400'>Copyright © Developed By |<span className='text-slate-600 bold hover:text-slate-800 duration-200'> Adil Patel</span></h1></a>
+            <div className='gap-4 flex items-center '>
+                
+                <a href="https://www.instagram.com/code_with_adil?igsh=NTc2dTRudnc1ZnYz" target='_blank' className='flex items-center specialBtni px-2 py-1 text-slate-500 rounded-full duration-200 hover:text-rose-400'>
+                    
+                <i className="fa-brands fa-instagram text-xl"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/adil-patel-737692252/" target='_blank' className='flex items-center specialBtn px-2 py-1 text-slate-500 rounded-full duration-200 hover:text-blue-600'>
+                    
+                <i className="fa-brands fa-linkedin text-xl"></i>
+                </a>
+                <a href="https://www.github.com/adil0710" target='_blank' className='flex items-center specialBtng px-2 py-1 text-slate-500 rounded-full duration-200 hover:text-slate-900'>
+                    
+                <i className="fa-brands fa-github text-xl"></i>
+                </a>
+            </div>
+        </header>
+  )
+}
+
+export default Footer
